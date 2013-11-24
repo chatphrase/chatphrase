52176d6bdfd4ab37980374faba73c1dab2412aee
Stuart P. Bentley <stuart@testtrack4.com>
2013-11-24 00:26:09 -0500

    New fancy RESTful signaling

--- static/chatphrase.js
+++ static/chatphrase.js
@@ -2,7 +2,8 @@
 /*global RTCPeerConnection getUserMedia attachMediaStream
   reattachMediaStream webrtcDetectedBrowser webrtcDetectedVersion
   RTCSessionDescription RTCIceCandidate*/
-
+//from signaling.js
+/*global chatphraseSignaling*/
 "use strict";
 
 // Create lowercase-and-hyphenated slug for a phrase.
@@ -20,7 +21,7 @@ function returnToLanding() {
   //TODO: kill the current connection state
   document.title = 'Chatphrase';
   document.getElementById("phrase").value = "";
-  document.getElementById('message').textContent = "";
+  setMessage("");
   switchState("landing");
 }
 
@@ -29,75 +30,8 @@ function resetPageState() {
   returnToLanding();
 }
 
-//because keeping track of a proper polling state is just too hard
-var enormousHackToStopPolling;
-
-// This is also hacky, but this time it's not my fault
-var persistentPeerConnectionReferenceToEvadeGarbageCollectionInChrome;
-
-// Constructs a function that posts ICE candidates.
-function icePoster(phrase, party) {
-  return function(evt) {
-    // After all the ICE candidates have been worked over,
-    // onicecandidate gets called with an event without a candidate.
-    // Send that so the other end knows when to stop ICE polling.
-    //if (evt.candidate) {
-      var iceRq = new XMLHttpRequest();
-       iceRq.onreadystatechange = function () {
-          if (iceRq.readyState == 4) {
-            //do nothing
-          }
-        };
-      iceRq.open("POST","/api/ice/"+phrase);
-      iceRq.setRequestHeader(
-        "Content-type", "application/json; charset=utf-8");
-      iceRq.send(JSON.stringify({party: party,
-        ic: evt.candidate ? evt.candidate : null}));
-    //}
-  };
-}
-
-// Constructor for function that adds candidates to a peer connection.
-// Useful for passing to the candidate queue's forEach.
-function candidateAdder(peercon) {
-  return function (candidate) {
-    peercon.addIceCandidate(candidate,
-      function() {console.log('added',candidate)},
-      function(err) {console.error(candidate,err)});
-  };
-}
-
-//Another hack, to not add ICE until the remote session has been set.
-var remoteIceQueue = [];
-
-function clearRemoteIceQueue(peercon) {
-  remoteIceQueue.forEach(candidateAdder(peercon));
-  remoteIceQueue = null;
-}
-
-//Function to handle incoming ICE candidates.
-function addIce(peercon, ice) {
-
-  // Add a candidate to the peercon, or queue it if we're queuing ICE
-  // candidates right now (because we haven't set the remote session
-  // description yet)
-  function addCandidate(candidate) {
-    if (remoteIceQueue) remoteIceQueue.push(candidate);
-    else candidateAdder(peercon)(candidate);
-  }
-
-  // Only some signals come with ICE, but we call this function on all signals.
-  if (ice) {
-    for (var i=0; i < ice.length; i++) {
-      if(ice[i])
-        addCandidate(new RTCIceCandidate(ice[i]));
-      // If there's a null in the ICE candidate array, there won't be any more
-      // candidates and we should stop polling for them.
-      else if(enormousHackToStopPolling) {
-        enormousHackToStopPolling.abort();
-      }
-    }
-  }
+function setMessage(text) {
+  document.getElementById('message').textContent = text;
 }
 
 // Switch which "page" element is currently visible.
@@ -113,211 +47,73 @@ function switchState(stateName) {
   newactive.classList.add('active');
 }
 
-function onRemoteStreamConnected(evt){
-  document.getElementById('message').textContent = "";
-  attachMediaStream(document.getElementById('vidscreen'),evt.stream);
-}
-
-//TODO: general XHR function, with timeout CB
-
-var consoleError = console.error.bind(console);
-
-function pollRing(phrase,body,peercon) {
-  document.getElementById('message').textContent = "Waiting...";
-  var pollRq = new XMLHttpRequest();
-   pollRq.onreadystatechange = function () {
-      if (pollRq.readyState == 4) {
-        // parse response
-        var resbody = JSON.parse(pollRq.responseText);
-
-        // Add or queue the ICE for the remote description
-        addIce(peercon,resbody.ice);
-
-        //If we have the remote answerer's session description
-        if (resbody.answer) {
-          // connect to the answer
-          document.getElementById('message').textContent = "Connecting...";
-
-          peercon.setRemoteDescription(
-            new RTCSessionDescription(resbody.answer),
-            function(){
-              console.log('connected to',resbody.answer);
-            },consoleError);
-
-          //stop queuing now that we've set the remote description
-          clearRemoteIceQueue(peercon);
-        }
-
-        // Keep ringing and gathering ICE candidates until the
-        // remote stream handler aborts the running poll
-        return pollRing(phrase, body, peercon);
-      }
-    };
-  pollRq.open("POST","/api/ring/"+phrase);
-  pollRq.setRequestHeader(
-    "Content-type", "application/json; charset=utf-8");
-
-  //allow the remote stream handler to terminate this
-  enormousHackToStopPolling = pollRq;
-  pollRq.send(body);
-}
-
-function answerRing(phrase,body,peercon) {
-  document.getElementById('message').textContent = "Connecting...";
-  var answerRq = new XMLHttpRequest();
-   answerRq.onreadystatechange = function () {
-      function ringFromDesc (f) {
-        return function (desc) {
-          return f(phrase,JSON.stringify({ "session": desc }));
-        };
-      }
-
-      if (answerRq.readyState == 4) {
-        //parse response
-        var resbody = JSON.parse(answerRq.responseText);
-
-        if(resbody.status == "answered") {
-          addIce(peercon,resbody.ice);
-          //inform the user that they're waiting for the other end
-          //to connect (if they haven't already)
-          //TODO: continue polling for ICE until connected?
-          //(final polls can terminate when the other end
-          //acknowledges it's connected)
-        } else {
-          //go back to the beginning
-          //TODO: destroy existing peer connection?
-          beginPhrase(phrase);
-        }
-      }
-    };
-  answerRq.open("POST","/api/answer/"+phrase);
-  answerRq.setRequestHeader(
-    "Content-type", "application/json; charset=utf-8");
-  answerRq.send(body);
-}
-
-function startRinging(phrase,stream) {
-  document.getElementById('message').textContent = "Creating connection...";
-  // Create a peer connection that will use
-  // vline's STUN server, Google's STUN server,
-  // and numb.viagenie.ca for TURN
-  var peercon = new RTCPeerConnection({
-    "iceServers": [
-        {"url": "stun:stun.vline.com"}
-        ,{"url": "stun:stun.l.google.com:19302"}
-        ,{
-            url: 'turn:numb.viagenie.ca',
-            username: 'ice@chatphrase.com',
-            credential: 'yovipletskickit'
-        }
-      ]},
-
-      // DTLS-SRTP is the future of WebRTC.
-      // Versions of Chrome before 31 don't do DTLS-SRTP by default
-      // (due to an inefficient certificate generation scheme or something),
-      // so you need to explicitly request it to be able to interoperate
-      // with other implementations (like Firefox).
-      {optional:[{DtlsSrtpKeyAgreement: true}]});
-
-  persistentPeerConnectionReferenceToEvadeGarbageCollectionInChrome = peercon;
-
-  //add our stream to the connection
-  peercon.addStream(stream);
-
-  //add a listener for the stream from the other end
-  peercon.onaddstream = onRemoteStreamConnected;
-
-  var firstRing = new XMLHttpRequest();
-   firstRing.onreadystatechange = function () {
-      function ringFromDesc (f) {
-        return function (desc) {
-          //TODO: I'm reading some stuff about how Opus should be listed
-          // as preferred at this point?
-
-          //I get why WebRTC works like this now (we can generate session
-          //descriptions that we don't actually end up using with no penalty).
-          //arg 2 is a NOP because Firefox freaks out if it's null, and the
-          //standard doesn't actually say it's optional.
-          peercon.setLocalDescription(desc, function(){}, consoleError);
-
-          //Send this request to the other end
-          return f(phrase,JSON.stringify(desc),peercon);
-        };
-      }
-
-      if (firstRing.readyState == 4) {
-        //parse response
-        var resbody = JSON.parse(firstRing.responseText);
-
-        peercon.onicecandidate = icePoster(phrase,
-          resbody.waiting ? 'answerer' : 'waiter');
-
-        // If the signal from the initial request came back that there's
-        // somebody waiting at this phrase
-        if (resbody.waiting) {
-
-          //add the remote session to the connection
-          peercon.setRemoteDescription(
-            new RTCSessionDescription(resbody.waiting),
-            function(){
-              console.log('connected to',resbody.waiting);
-            }, consoleError);
-
-          //Add any ICE candidates attached to this request
-          addIce(peercon,resbody.ice);
-
-          // Process the queue we just made
-          clearRemoteIceQueue(peercon);
-
-          //Send our answer to that waiting session
-          peercon.createAnswer(ringFromDesc(answerRing));
-        } else {
-          peercon.createOffer(ringFromDesc(pollRing),consoleError,
-            {
-              mandatory: {
-                OfferToReceiveAudio: true,
-                OfferToReceiveVideo: true
-                },
-              optional: []
-              });
-        }
-      }
-    };
-  firstRing.open("GET","/api/ring/"+phrase);
-  firstRing.setRequestHeader(
-    "Content-type", "application/json; charset=utf-8");
-  firstRing.send();
-}
+var signal;
+var connected;
+var sigHooks = {
+  error: function (err) {
+    setMessage('ERROR: ' + err.message);
+    console.error(err);
+  },
+  remoteStream: function onRemoteStream(stream){
+    setMessage("");
+    attachMediaStream(document.getElementById('vidscreen'),stream);
+    // TODO: implement a proper way of checking the ICE Agent connection state
+    // especially since I think WebRTC implements a dummy / dud stream until
+    // both ends are truly connected
+    connected = stream;
+  },
+  remoteSignalLost: function restartIfNotConnected() {
+    // If we never received the remote stream
+    if (!connected) {
+      // Restart signaling
+      signal.start();
+    }
+  }
+};
 
 function beginPhrase(phrase) {
+  // Ready the signaling state
+  signal = chatphraseSignaling(phrase,sigHooks);
+
+  // Update the window title
   document.title = phrase.replace(/-/g,' ') + ' : Chatphrase';
-  getUserMedia({audio:true,video:true},function(stream){
+
+  // Attempt to acquire the user's camera
+  getUserMedia({audio:true,video:true},
+    gumSuccess, gumError);
+
+  function gumSuccess(stream) {
+    // Display the local video feed in a picture-in-picture element
     attachMediaStream(document.getElementById('pip'),stream);
-    //advance to lobby / connection
-    startRinging(phrase,stream);
-  },function(err){
+
+    // Begin connecting / waiting
+    setMessage("Creating connection...");
+    signal.start(stream);
+  }
+
+  function gumError(err) {
     if(err.code && err.code == err.PERMISSION_DENIED
       || err.name == "PERMISSION_DENIED"
     ){
-      document.getElementById('message').textContent =
+      setMessage(
         "Permission to use your camera and microphone has been denied. "+
         "If you choose to deny permission, reset permissions for "+
         "camera and microphone access on chatphrase.com. "+
         "If you did not choose to deny permission, check that another "+
         "program isn't using the camera and/or microphone. "+
         "Once your camera and microphone are ready, refresh the "+
-        "page to retry the chat connection.";
+        "page to retry the chat connection.");
     } else {
-      document.getElementById('message').textContent =
-        "Recieved error trying to getUserMedia: " +
-          (typeof err == "string" ? err : JSON.stringify(err)) ;
+      setMessage("Error trying to getUserMedia: " +
+        (typeof err == "string" ? err : JSON.stringify(err))) ;
     }
-  });
+  }
 
-  //switch to limbo until media is successfully gotten
-  document.getElementById('message').textContent =
-    "Chatphrase will begin connecting once you grant permission to use your "
-    + "camera and microphone...";
+  // Inform the user what they need to do
+  setMessage("Chatphrase will begin connecting once you grant permission "
+    + "to use your camera and microphone...");
+
+  // Enter the room state
   switchState("room");
 }
 
---
+++ static/signaling.js
@@ -0,0 +1,217 @@
+//from adapter.js
+/*global RTCPeerConnection getUserMedia attachMediaStream
+  reattachMediaStream webrtcDetectedBrowser webrtcDetectedVersion
+  RTCSessionDescription RTCIceCandidate*/
+
+"use strict";
+
+var chatphraseIceServers = [
+  {"url": "stun:stun.vline.com"}
+  ,{"url": "stun:stun.l.google.com:19302"}
+  ,{
+      url: 'turn:numb.viagenie.ca',
+      username: 'ice@chatphrase.com',
+      credential: 'yovipletskickit'
+  }
+];
+
+// The base of the URLs to check the initial offers from.
+var chatphraseOffersBase = "/signal/offers/";
+
+//phrase: the slugified phrase to connect to.
+//cbs: The callbacks to send to the UI/system on various events/hooks.
+function chatphraseSignaling (slugPhrase, cbs) {
+  cbs = cbs || {};
+  var iface = {
+    on: cbs
+  };
+
+  function xhrPostSdp(url, body, cb, opts) {
+    var rq = new XMLHttpRequest();
+    rq.onreadystatechange = function () {
+      if (rq.readyState == 4) {
+        return cb(rq.status, rq.responseText,
+          rq.getResponseHeader('Location'));
+      }
+    };
+    rq.onerror = function(e){cb(null,e)};
+    rq.open("POST", url);
+    if (opts.timeout) {
+      rq.timeout = opts.timeout;
+      rq.ontimeout = opts.ontimeout;
+    }
+    rq.setRequestHeader("Content-Type", "text/sdp; charset=utf-8");
+    rq.send(body);
+  }
+
+  function xhrPutSdp(url, body, cb, opts) {
+    var rq = new XMLHttpRequest();
+    rq.onreadystatechange = function () {
+      if (rq.readyState == 4) {
+        return cb(rq.status, rq.responseText,
+          // PUTs shouldn't really involve Location headers, but
+          rq.getResponseHeader('Location'));
+      }
+    };
+    rq.onerror = function(e){cb(null,e)};
+    rq.open("PUT", url);
+    if (opts.timeout) {
+      rq.timeout = opts.timeout;
+      rq.ontimeout = opts.ontimeout;
+    }
+    rq.setRequestHeader("Content-Type", "text/sdp; charset=utf-8");
+    rq.send(body);
+  }
+
+  // The etag of the latest SDP we've received.
+  var ourtag;
+
+  //This isn't STRICTLY SDP-specific, but I'm keeping the name for symmetry.
+  function xhrGetSdp(url, cb, opts) {
+    var rq = new XMLHttpRequest();
+    rq.onreadystatechange = function () {
+      if (rq.readyState == 4) {
+        return cb(rq.status,rq.responseText,
+          rq.getResponseHeader('Location'), rq.getResponseHeader('Etag'));
+      }
+    };
+    rq.onerror = function(e){cb(null,e)};
+    rq.open("GET", url);
+    if (opts.timeout) {
+      rq.timeout = opts.timeout;
+      rq.ontimeout = opts.ontimeout;
+    }
+    rq.setRequestHeader("Content-Type", "text/sdp; charset=utf-8");
+    if (opts.ourtag){
+      rq.setRequestHeader("If-None-Match", opts.ourtag);
+    }
+    rq.send();
+  }
+
+  // The RTCPeerConnection.
+  var pc;
+  // The path we GET to listen on / PUT updates to.
+  var signalPath;
+  // The local stream, for restarting.
+  var localStream;
+
+  //stream: the stream object from getUserMedia.
+  iface.start = function start(stream) {
+
+    if (stream) localStream = stream;
+
+    // Re-initialize our outside state variables
+    ourtag = undefined;
+    signalPath = undefined;
+
+    pc = new RTCPeerConnection({
+      "iceServers": chatphraseIceServers},
+
+      // DTLS-SRTP is required by the current WebRTC standard.
+      // Versions of Chrome before 31 don't do DTLS-SRTP by default
+      // (due to an inefficient certificate generation scheme or something),
+      // so you need to explicitly request it to be able to interoperate
+      // with other implementations (like Firefox).
+      {optional:[{DtlsSrtpKeyAgreement: true}]});
+
+    // Add our stream to the connection
+    pc.addStream(localStream);
+
+    // Add a listener for the stream from the other end
+    pc.onaddstream = onAddStream;
+
+    // Add our ICE listener
+    pc.onicecandidate = onIceCandidate;
+
+    getOffer();
+  };
+
+  function getOffer() {
+    var endpoint = chatphraseOffersBase + slugPhrase;
+
+    xhrGetSdp(endpoint, beginConnection);
+
+    function beginConnection(status,body,location) {
+      if (status == 200) {
+        pc.setRemoteDescription(new RTCSessionDescription({sdp: body,
+          type: "offer"}), answerOffer, onError);
+      // If there's no current offer
+      } else if (status == 404) {
+        // Create the offer
+        pc.createOffer(setLocalAndPost(location), onError, {
+          mandatory: {
+            OfferToReceiveAudio: true,
+            OfferToReceiveVideo: true },
+          optional: []});
+      } else if(status) {
+        onError(new Error(
+          "Got status "+status+" from "+endpoint+": "+body));
+      } else {
+        onError(body);
+      }
+    }
+  }
+
+  function answerOffer(location) {
+    pc.createAnswer(setLocalAndPost(location), onError, {
+      mandatory: {},
+      optional: []});
+  }
+
+  function setLocalAndPost(location) {
+    return function(desc) {
+      pc.setLocalDescription(desc, function(){
+        xhrPostSdp(location, desc.sdp, continueFromPost(location),
+          onError);
+      }, onError);
+    };
+  }
+
+  function continueFromPost(location) {
+    // Ignore the "nulLocation" parameter, we never expect to get it.
+    function poller(status, body, nulLocation, etag){
+      if (status == 204 || status == 304) {
+        // no change, continue polling
+        xhrGetSdp(location, poller, {ourtag: ourtag});
+      } else if (status == 200) {
+        pc.setRemoteDescription(new RTCSessionDescription({sdp: body,
+          type: pc.remoteDescription ? pc.remoteDescription.type : "answer"}));
+        ourtag = etag;
+      } else if (status == 404) {
+        cbs.remoteSignalLost && cbs.remoteSignalLost();
+      } else if (status) {
+        onError(new Error (
+          "Recieved status " + status + " while polling " + location
+            + ": " + body));
+      } else {
+        onError(body);
+      }
+    }
+    xhrGetSdp(location, poller, {ourtag: ourtag});
+  }
+
+  function onError(err) {
+    cbs.error && cbs.error(err);
+  }
+
+  function onAddStream(evt) {
+    cbs.remoteStream && cbs.remoteStream(evt.stream);
+  }
+
+  function onIceCandidate(candidate) {
+    //if the server is ready to receive our updates
+    if (signalPath) {
+      // Update our session description with the new ICE candidates
+      xhrPutSdp(signalPath, pc.localDescription.sdp, function (status,body) {
+        if (!status) {
+          onError(body);
+        } else if (status <= 500) {
+          onError(new Error(
+            'Received status ' + status + ' updating SDP: ' + body));
+        }
+      });
+    }
+  }
+
+  return iface;
+}
