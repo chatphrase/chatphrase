321fbaee7a44ba318f552ecc867a9531867172b5
Stuart P. Bentley <stuart@testtrack4.com>
2013-12-10 23:08:22 -0500

    Update signaling to post and poll with message lists

--- static/signaling.js
+++ static/signaling.js
@@ -26,7 +26,7 @@ function chatphraseSignaling (slugPhrase, cbs) {
     on: cbs
   };
 
-  function xhrPostSdp(url, body, cb, opts) {
+  function xhrPostJson(url, body, cb, opts) {
     opts = opts || {};
     var rq = new XMLHttpRequest();
     rq.onreadystatechange = function () {
@@ -41,42 +41,30 @@ function chatphraseSignaling (slugPhrase, cbs) {
       rq.timeout = opts.timeout;
       rq.ontimeout = opts.ontimeout;
     }
-    rq.setRequestHeader("Content-Type", "text/sdp; charset=utf-8");
-    rq.send(body);
+    // NOTE: the type is text/json and not application/json because I'm
+    // too lazy and paranoid to do things right on the server.
+    // (It's currently only designed to parse the body when it's a
+    // text/ type... eek.)
+    // This should be fixed at some point down the line, for instance,
+    // once caress is updated with Content-Type saving.
+    rq.setRequestHeader("Content-Type", "text/json; charset=utf-8");
+    rq.send(JSON.stringify(body));
   }
 
-  function xhrPutSdp(url, body, cb, opts) {
-    opts = opts || {};
-    var rq = new XMLHttpRequest();
-    rq.onreadystatechange = function () {
-      if (rq.readyState == 4) {
-        return cb(rq.status, rq.responseText,
-          // PUTs shouldn't really involve Location headers, but
-          rq.getResponseHeader('Location'));
-      }
-    };
-    rq.onerror = function(e){cb(null,e)};
-    rq.open("PUT", url);
-    if (opts.timeout) {
-      rq.timeout = opts.timeout;
-      rq.ontimeout = opts.ontimeout;
-    }
-    rq.setRequestHeader("Content-Type", "text/sdp; charset=utf-8");
-    rq.send(body);
-  }
-
-  // The etag of the latest SDP we've received.
-  var ourtag;
-
-  //This isn't STRICTLY SDP-specific, but I'm keeping the name for symmetry.
-  function xhrGetSdp(url, cb, opts) {
+  function xhrGetJson(url, cb, opts) {
     opts = opts || {};
     var rq = new XMLHttpRequest();
     function handleEnd() {
     }
     rq.onreadystatechange = function () {
       if (rq.readyState == 4) {
-        return cb(rq.status,rq.responseText,
+        var body;
+        try {
+          body = JSON.parse(rq.responseText);
+        } catch(e) {
+          body = rq.responseText;
+        }
+        return cb(rq.status, body,
           rq.getResponseHeader('Location'), rq.getResponseHeader('Etag'));
       }
     };
@@ -86,7 +74,6 @@ function chatphraseSignaling (slugPhrase, cbs) {
       rq.timeout = opts.timeout;
       rq.ontimeout = opts.ontimeout;
     }
-    rq.setRequestHeader("Content-Type", "text/sdp; charset=utf-8");
     if (opts.ourtag){
       rq.setRequestHeader("If-None-Match", opts.ourtag);
     }
@@ -95,8 +82,10 @@ function chatphraseSignaling (slugPhrase, cbs) {
 
   // The RTCPeerConnection.
   var pc;
-  // The path we GET to listen on / PUT updates to.
+  // The path we GET to listen on / POST updates to.
   var signalPath;
+  // The index of the current message we're asking for.
+  var pollPoint = 0;
   // The local stream, for restarting.
   var localStream;
 
@@ -106,7 +95,6 @@ function chatphraseSignaling (slugPhrase, cbs) {
     if (stream) localStream = stream;
 
     // Re-initialize our outside state variables
-    ourtag = undefined;
     signalPath = undefined;
 
     pc = new RTCPeerConnection({
@@ -117,6 +105,10 @@ function chatphraseSignaling (slugPhrase, cbs) {
       // (due to an inefficient certificate generation scheme or something),
       // so you need to explicitly request it to be able to interoperate
       // with other implementations (like Firefox).
+
+      // This is less relevant since Chrome 31 went stable in
+      // mid-November 2013, but it doesn't hurt anything to keep it around,
+      // at least for a while.
       {optional:[{DtlsSrtpKeyAgreement: true}]});
 
     // Add our stream to the connection
@@ -134,12 +126,18 @@ function chatphraseSignaling (slugPhrase, cbs) {
   function getOffer() {
     var endpoint = chatphraseOffersBase + slugPhrase;
 
-    xhrGetSdp(endpoint, beginConnection);
+    xhrGetJson(endpoint, beginConnection);
 
     function beginConnection(status,body,location) {
       if (status == 200) {
-        pc.setRemoteDescription(new RTCSessionDescription({sdp: body,
-          type: "offer"}), answerOffer(location), onError);
+
+        // mark that we've got the initial content, and will want to start
+        // asking for ICE messages
+        ++pollPoint;
+
+        // Set the remote offer and move on to answer it back
+        pc.setRemoteDescription(new RTCSessionDescription(body),
+          answerOffer(location), onError);
       // If there's no current offer
       } else if (status == 404) {
         // Create the offer
@@ -168,7 +166,7 @@ function chatphraseSignaling (slugPhrase, cbs) {
   function setLocalAndPost(location) {
     return function(desc) {
       pc.setLocalDescription(desc, function(){
-        xhrPostSdp(location, desc.sdp, continueFromPost, onError);
+        xhrPostJson(location, desc, continueFromPost, onError);
       }, onError);
     };
   }
@@ -177,7 +175,7 @@ function chatphraseSignaling (slugPhrase, cbs) {
 
     function poll() {
       // continue polling
-      xhrGetSdp(location+'?side=down', respondToPoll, {ourtag: ourtag});
+      xhrGetJson(location + '/' + pollPoint + '?side=down', respondToPoll);
     }
 
     // Ignore the "nulLocation" parameter, we never expect to get it.
@@ -190,71 +188,75 @@ function chatphraseSignaling (slugPhrase, cbs) {
 
       // if there's a new body
       } else if (status == 200) {
-
-        // save the identifier for this new body
-        ourtag = etag;
-
-        // update our peerconnection with the new remote description,
-        // then continue polling
-        pc.setRemoteDescription(new RTCSessionDescription({sdp: body,
-          type: pc.remoteDescription ? pc.remoteDescription.type : "answer"}),
-          poll, onError);
+        // If we've been awaiting the initial remote description
+        if (pollPoint == 0) {
+          // update our peerconnection with the new remote description,
+          // then continue polling
+          ++pollPoint;
+          return pc.setRemoteDescription(new RTCSessionDescription(body),
+            poll, onError);
+        // If we're waiting for ICE candidate updates
+        } else {
+          // Add remote ICE candidate
+          ++pollPoint;
+          return pc.addIceCandidate(new RTCIceCandidate(body), poll, onError);
+        }
 
       // if the body is gone (not found, because we don't special-case them)
       } else if (status == 404) {
         // fire the event for that
-        cbs.remoteSignalLost && cbs.remoteSignalLost();
+        return cbs.remoteSignalLost && cbs.remoteSignalLost();
 
       // if there was some unexpected status
       } else if (status) {
         // report it as an error
-        onError(new Error (
+        return onError(new Error (
           "Got status " + status + " while polling " + location
             + ": " + body));
 
       // if there was an error in the XHR (no status)
       } else {
         // report it
-        onError(body);
+        return onError(body);
       }
     }
 
     // If the signal endpoint was created
     if (status == 201) {
-      // Export the path we've just been given for PUTting updates to
+      // Export the path we've just been given for POSTing updates to
       signalPath = location;
       // Start polling
-      poll();
+      return poll();
 
     // If we're colliding or the endpoint is missing
     // (the endpoint goes missing when someone else answers an offer first)
     } else if (status == 303 || status == 404) {
       // Re-get the offer
-      getOffer();
+      return getOffer();
 
     } else if (status) {
-      onError(new Error (
+      return onError(new Error (
         "Got status " + status + " posting session: " + body));
 
     } else {
-      onError(body);
+      return onError(body);
     }
 
   }
 
   function onError(err) {
-    cbs.error && cbs.error(err);
+    return cbs.error && cbs.error(err);
   }
 
   function onAddStream(evt) {
-    cbs.remoteStream && cbs.remoteStream(evt.stream);
+    return cbs.remoteStream && cbs.remoteStream(evt.stream);
   }
 
   function onIceCandidate(candidate) {
     //if the server is ready to receive our updates
     if (signalPath) {
       // Update our session description with the new ICE candidates
-      xhrPutSdp(signalPath+'?side=up', pc.localDescription.sdp,
+      xhrPostJson(signalPath+'?side=up', candidate,
         function (status,body) {
           if (!status) {
             onError(body);
